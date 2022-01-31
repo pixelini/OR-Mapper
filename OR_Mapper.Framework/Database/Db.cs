df1edd549155e8b59c50d6581132e3e0c8b8a2da
pixelini <eglatz@gmx.at>
2022-01-31 19:08:26 +0100

    small fix

--- OR_Mapper.Framework/Database/Db.cs
+++ OR_Mapper.Framework/Database/Db.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
+﻿using System.Data;
 using Npgsql;
 using OR_Mapper.Framework.Caching;
 using OR_Mapper.Framework.Exceptions;
